79f1716bcbd8cf5789c71609d21e9a81b73f2df7
Israel Correa <raeldev@outlook.com>
2023-07-15 16:24:39 -0300

    Criação do TransactionModel

---
+++ src/app/transaction/transactionModel.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize')
+const database = require('../../config/database')
+
+module.exports = database.define('transaction', {
+  label: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  value: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  categories: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dueDate: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  type: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+})
