a941e2d21236a9921b99728d86ffcc5aff3065e2
Israel Correa <raeldev@outlook.com>
2023-07-15 16:21:42 -0300

    Instalando sequelize, sqlite3 e configurando conexao com o banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize')
+
+const database = new Sequelize({
+  dialect: 'sqlite',
+  storage: process.env.SQLITE_STORAGE,
+})
+
+database
+  .sync()
+  .then((res) => console.log('Você está conectado com o banco de dados: ', res))
+  .catch((err) =>
+    console.log('Não foi possivel conectar no banco de dados: ', err)
+  )
+
+module.exports = database
