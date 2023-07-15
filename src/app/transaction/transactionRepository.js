e39d3587ae34330cd9552cd98deda2241f67a9c0
Israel Correa <raeldev@outlook.com>
2023-07-15 16:25:38 -0300

    Criando o TransactionRepository

---
+++ src/app/transaction/transactionRepository.js
@@ -0,0 +1,34 @@
+const transactionModel = require('./transactionModel')
+
+module.exports = (() => {
+  const addTransaction = async (transactionDto) => {
+    console.log('Add Transaction Values', transactionDto)
+    return await transactionModel.create(transactionDto)
+  }
+
+  const editTransaction = async (transactionDto) => {
+    return await transactionModel.update(transactionDto, {
+      where: { id: transactionDto.id },
+    })
+  }
+
+  const deleteTransaction = async (id) => {
+    return await transactionModel.destroy({ where: { id: id } })
+  }
+
+  const findAllTransaction = async () => {
+    return await transactionModel.findAll()
+  }
+
+  const findTransactionById = async (id) => {
+    return await transactionModel.findOne({ where: { id } })
+  }
+
+  return {
+    addTransaction,
+    editTransaction,
+    deleteTransaction,
+    findAllTransaction,
+    findTransactionById,
+  }
+})()
