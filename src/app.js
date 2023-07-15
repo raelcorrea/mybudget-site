ed2140e4d15299867e2030ca3606c7452132c7bb
Israel Correa <raeldev@outlook.com>
2023-07-15 19:20:39 -0300

    Refatorando as rotas criando um middleware e alterando a indexRouter e transactionRouter

--- src/app.js
+++ src/app.js
@@ -6,14 +6,13 @@ const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const hbs = require('hbs')
 
-const indexRouter = require('./routes/index')
 const errorHandlerMiddleware = require('./middleware/errorHandlerMiddleware')
 const hbsRegisterHelpersMiddleware = require('./middleware/hbsRegisterHelpersMiddleware')
+const routerMiddleWare = require('./middleware/routerMiddleWare')
 
 hbsRegisterHelpersMiddleware(hbs)
 
 const app = express()
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
@@ -24,7 +23,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.use('/', indexRouter)
+routerMiddleWare(app)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
