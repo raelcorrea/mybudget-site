b1d7f36159be15d67351a52d62f49c16ab84d953
Israel Correa <raeldev@outlook.com>
2023-07-16 01:46:37 -0300

    Melhorias no layout da lista e formularios de transação

--- src/middleware/hbsRegisterHelpersMiddleware.js
+++ src/middleware/hbsRegisterHelpersMiddleware.js
@@ -1,3 +1,5 @@
+const currency = require('../utils/currency')
+
 module.exports = (hbs) => {
   hbs.registerHelper('isEqual', function (expectedValue, value) {
     return value === expectedValue
@@ -6,7 +8,24 @@ module.exports = (hbs) => {
   hbs.registerHelper('isEmpty', function (value) {
     return value === undefined || value === ''
   })
+
   hbs.registerHelper('isNotEmpty', function (value) {
     return value !== undefined && value !== ''
+  })
+
+  hbs.registerHelper('toDateString', (date) => {
+    return new Date(date).toDateString()
+  })
+
+  hbs.registerHelper('toCurrencyBrl', (value) => {
+    return currency(value).BRL()
+  })
+
+  hbs.registerHelper('toArray', (value) => {
+    if (value != undefined && value != '') {
+      return value.split(value)
+    }
+
+    return []
   })
 }
