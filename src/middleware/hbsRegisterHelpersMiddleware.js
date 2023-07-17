69116031a4f5debc6ea5ff8aa2041401f8ba7000
Israel Correa <raeldev@outlook.com>
2023-07-17 09:26:07 -0300

    Exibindo as despesas como numeros negativos

--- src/middleware/hbsRegisterHelpersMiddleware.js
+++ src/middleware/hbsRegisterHelpersMiddleware.js
@@ -3,6 +3,7 @@ const hbsIsNotEmptyRegisterHelper = require('../helpers/hbsIsNotEmptyRegisterHel
 const hbsToCurrencyBrlRegisterHelper = require('../helpers/hbsToCurrencyBrlRegisterHelper')
 const hbsToDateSysRegisterHelper = require('../helpers/hbsToDateSysRegisterHelper')
 const hbsToDateRegisterHelper = require('../helpers/hbsToDateRegisterHelper')
+const hbsIsLowerThenZero = require('../helpers/hbsIsLowerThenZero')
 
 module.exports = (hbs) => {
   hbs.registerHelper('isEqual', hbsIsEqualRegisterHelper)
@@ -10,4 +11,5 @@ module.exports = (hbs) => {
   hbs.registerHelper('toDate', hbsToDateRegisterHelper)
   hbs.registerHelper('toDateSys', hbsToDateSysRegisterHelper)
   hbs.registerHelper('toCurrencyBrl', hbsToCurrencyBrlRegisterHelper)
+  hbs.registerHelper('isLowerThenZero', hbsIsLowerThenZero)
 }
--- src/partials/balancePartial.js
+++ src/partials/balancePartial.js
@@ -14,7 +14,7 @@ module.exports = async (transactionService) => {
     }
   })
 
-  const total = incomes - expenses
+  const total = incomes + expenses
 
   return {
     incomes: currencyHelper(incomes).BRL(),
